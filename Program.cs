16ecacfdf11e968061b49272e5a2a51f6bc74cc3
Your Name <you@example.com>
2020-12-13 22:16:59 -0300

    soma

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace soma2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s1, s2, sf;
+
+            Console.WriteLine("Ola vamos fazer a soma de 2 numeros");
+
+            Console.Write("Qual é o 1ºValor: ");
+            s1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Qual é  o 2ºValor: ");
+            s2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            sf = s1 + s2;
+            Console.WriteLine($"A soma de {s1} + {s2} = {sf}");
+            Console.WriteLine();
+        }
+    }
+}
